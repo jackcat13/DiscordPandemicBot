d6d410398fb9d2367a1d44a7bf022b89f21b0716
jackcat13 <christophehenry11@gmail.com>
2021-01-22 02:09:11 +0100

    Update JDA version

--- pandemicBot/src/main/java/com/corona/virus/PandemicBotMain.java
+++ pandemicBot/src/main/java/com/corona/virus/PandemicBotMain.java
@@ -3,27 +3,28 @@ package com.corona.virus;
 import com.corona.virus.game.GameStatus;
 import com.corona.virus.game.PandemicGame;
 import com.corona.virus.game.Player;
-import net.dv8tion.jda.core.AccountType;
-import net.dv8tion.jda.core.JDA;
-import net.dv8tion.jda.core.JDABuilder;
-import net.dv8tion.jda.core.entities.Message;
-import net.dv8tion.jda.core.entities.MessageChannel;
-import net.dv8tion.jda.core.entities.User;
-import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class PandemicBotMain extends ListenerAdapter {
 
-    public static final String TOKEN = "NjkwMjEyNzk1Mzk1ODY2NjM0.XnTw3g.Jefy6WcHI-nXFHzUF-5lO7h-Phk";
+    public static final String TOKEN = "NjkwMjEyNzk1Mzk1ODY2NjM0.XnOIwQ.kcQ5RfttoF54bIyaD4mmTlfkiPc";
     public static final String START_PANDEMIC = "!startPandemic";
     public static final String START_PANDEMIC_MESSAGE = "Pandemic is there. Users in this server will be coronned. Fear my almighty power!!! (To visualize the scores, you can use the !score command)";
     public static final String HEAL_MESSAGE = "!heal";
     public static final String SCORE_MESSAGE = "!score";
-    public static final String PANDEMIC_CHANNEL = "général";
+    public static final String PANDEMIC_CHANNEL = "pandemic";
     public static final String CORONNED_MESSAGE = " is coronned. First player to heal him will increase his score. Use command !heal to do so.";
     public static final String EFFECTIVE_HEAL_MESSAGE = "Effective heal performed. Scores: \n";
 
@@ -32,10 +33,9 @@ public class PandemicBotMain extends ListenerAdapter {
     private static JDA jda;
 
     public static void main(String[] args) throws LoginException {
-        JDABuilder jdbBuilder = new JDABuilder(AccountType.BOT);
-        jdbBuilder.setToken(TOKEN);
-        jdbBuilder.addEventListener(new PandemicBotMain());
-        jda = jdbBuilder.buildAsync();
+        jda = JDABuilder.createDefault(TOKEN)
+                .addEventListeners(new PandemicBotMain())
+                .build();
     }
 
     @Override
@@ -45,7 +45,7 @@ public class PandemicBotMain extends ListenerAdapter {
             return;
         }
         else if(inputMessage.equals(START_PANDEMIC)){
-            startGameEvent(event.getChannel(), event.getJDA().getUsers());
+            startGameEvent(event.getChannel(), jda.getUsers());
         }
         else if(inputMessage.equals(HEAL_MESSAGE)){
             healPlayers(event);
@@ -88,7 +88,7 @@ public class PandemicBotMain extends ListenerAdapter {
     private void gameLoop() {
         if (pandemicGame.getGameStatus() == GameStatus.IN_PROGRESS){
             List<Player> players = pandemicGame.getPlayers();
-            Player player = players.get(ThreadLocalRandom.current().nextInt(0, players.size()-1));
+            Player player = players.get(ThreadLocalRandom.current().nextInt(players.size()));
             if (pandemicGame.isUserCoronned(player.getId())){
                 jda.getTextChannelsByName(PANDEMIC_CHANNEL, true).get(0).sendMessage(player.getId() + CORONNED_MESSAGE).queue();
             }
